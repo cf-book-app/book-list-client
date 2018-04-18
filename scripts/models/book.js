b0d1b0b3d9faedcde1923de9fe880a66003d6c16
JB Tellez <jb.tellez@gmail.com>
2018-04-17 20:21:04 -0700

    In class

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,51 +1,62 @@
 const ENV = {};
 
 ENV.isProduction = window.location.protocol === 'https:';
-ENV.productionApiUrl = 'https://jb-301n10-booklist.herokuapp.com';
+
+ENV.productionApiUrl = '';
+
 ENV.developmentApiUrl = 'http://localhost:3000';
+
 ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
 
 // if running locally with hybrid in query string
+
 // then force use of production API
+
 var urlParams = new URLSearchParams(window.location.search);
+
 if(urlParams.has('hybrid')) {
+
     ENV.apiUrl = ENV.productionApiUrl;
 }
 
+
 var app = app || {};
 
-(module => {
+(function(module) {
 
-    module.Book = Book;
+    function Book(rawDataObj) {
 
-    function errorCallback(err) {
-        console.error(err);
-        module.errorView.initErrorPage(err);
-      }
-      
-    function Book(bookObj) {
-        Object.assign(this, bookObj);
+        // this.author = rawDataObj.author;
+        // this.title = rawDataObj.title;
+        // this.image_url = rawDataObj.image_url;
+        // this.description = rawDataObj.description;
+        // this.isbn = rawDataObj.isbn;
+
+        Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
     }
 
     Book.prototype.toHtml = function() {
-        const template = Handlebars.compile($('#book-list-template').text());
+        let template = Handlebars.compile($('#book-list-template').text());
         return template(this);
     }
 
-    Book.all = [];
-
-    Book.loadAll = function(rows) {
-        Book.all = rows.map(row => new Book(row));
-    } 
+    
+    Book.loadAll = function(books) {
+        Book.all = books.sort((a,b) => b.title - a.title)
+            .map(rawBook => new Book(rawBook));
+    }
 
     Book.fetchAll = function(callback) {
+
         $.getJSON(ENV.apiUrl + '/api/v1/books')
-            .then(Book.loadAll)
-            .then(callback)
-            .catch(errorCallback); 
+            .then(books => {
+                Book.loadAll(books);
+            });
+
+        
     }
-    
 
-})(app)
+    module.Book = Book;
 
-$(() => app.Book.fetchAll(app.bookView.initPage));
+})(app);
+
